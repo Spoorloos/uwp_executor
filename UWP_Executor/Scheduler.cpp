050d87fba032d8a7d6b3a3408ce95f38e1401652
mickey <59575072+Spoorloos@users.noreply.github.com>
2023-09-16 21:21:23 +0200

    Minor improvements.

--- UWP_Executor/Scheduler.cpp
+++ UWP_Executor/Scheduler.cpp
@@ -6,14 +6,16 @@
 
 uintptr_t Scheduler::get_job(const char* job_name) noexcept {
 	// Get the scheduler.
-	const auto scheduler = Roblox::get_scheduler();
+	static auto scheduler = uintptr_t();
+	if (!scheduler)
+		scheduler = Roblox::get_scheduler();
 
 	// Get the start and end address of the job-vector.
 	auto jobs_start = *reinterpret_cast<uintptr_t*>(scheduler + 0x134);
 	const auto jobs_end = *reinterpret_cast<uintptr_t*>(scheduler + 0x138);
 
 	// Loop through the job pointers (each 8 bytes long as they're shared_ptrs).
-	auto result = NULL;
+	auto result = uintptr_t();
 	for (; jobs_start < jobs_end; jobs_start += 8u) {
 		const auto job = *reinterpret_cast<uintptr_t*>(jobs_start);
 
@@ -25,13 +27,11 @@ uintptr_t Scheduler::get_job(const char* job_name) noexcept {
 }
 
 uintptr_t Scheduler::get_script_context() {
-	// Get the WaitingHybridScriptsJob.
-	const auto job = Scheduler::get_job("WaitingHybridScriptsJob");
-
-	// If it couldn't find the job, throw an exception.
-	if (job == NULL)
+	// Get the WaitingHybridScriptsJob and make sure it was found.
+	if (const auto job = Scheduler::get_job("WaitingHybridScriptsJob"))
+		// Return the script context.
+		return *reinterpret_cast<uintptr_t*>(job + 0x1A8);
+	else
+		// If it couldn't find the job, throw an exception.
 		throw std::runtime_error("Couldn't find WaitingHybridScriptsJob");
-
-	// Return the script context.
-	return *reinterpret_cast<uintptr_t*>(job + 0x1A8);
 }
